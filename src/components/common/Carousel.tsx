63fba6e0416d4ed71c662fcd984b6afbc214b4d5
Sina Zinsaz <sinazinsaz1@gmail.com>
2024-10-26 14:45:51 +0330

    completed carousel

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -8,7 +8,7 @@ import "swiper/css/pagination";
 import "../../styles/carousel.css";
 
 import { Pagination } from "swiper/modules";
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function Carousel() {
   return (
@@ -16,15 +16,143 @@ export default function Carousel() {
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
           <Box component="img" src={img1} alt="img1" />
+          <Box
+            sx={{
+              position: "absolute",
+              left: 0,
+              textAlign: "left",
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: 2,
+              pl: 7,
+            }}
+          >
+            <Typography variant="h4">Gold big hoops</Typography>
+            <Typography variant="h5">$ 68,00</Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                width: "70%",
+                fontWeight: "700",
+                fontSize: "16px",
+                color: "white",
+                border: "2px solid white",
+                "&:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              View Product
+            </Button>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
           <Box component="img" src={img1} alt="img1" />
+          <Box
+            sx={{
+              position: "absolute",
+              left: 0,
+              textAlign: "left",
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: 2,
+              pl: 7,
+            }}
+          >
+            <Typography variant="h4">Gold big hoops</Typography>
+            <Typography variant="h5">$ 68,00</Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                width: "70%",
+                fontWeight: "700",
+                fontSize: "16px",
+                color: "white",
+                border: "2px solid white",
+                "&:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              View Product
+            </Button>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
           <Box component="img" src={img1} alt="img1" />
+          <Box
+            sx={{
+              position: "absolute",
+              left: 0,
+              textAlign: "left",
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: 2,
+              pl: 7,
+            }}
+          >
+            <Typography variant="h4">Gold big hoops</Typography>
+            <Typography variant="h5">$ 68,00</Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                width: "70%",
+                fontWeight: "700",
+                fontSize: "16px",
+                color: "white",
+                border: "2px solid white",
+                "&:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              View Product
+            </Button>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
           <Box component="img" src={img1} alt="img1" />
+          <Box
+            sx={{
+              position: "absolute",
+              left: 0,
+              textAlign: "left",
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: 2,
+              pl: 7,
+            }}
+          >
+            <Typography variant="h4">Gold big hoops</Typography>
+            <Typography variant="h5">$ 68,00</Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                textTransform: "none",
+                width: "70%",
+                fontWeight: "700",
+                fontSize: "16px",
+                color: "white",
+                border: "2px solid white",
+                "&:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              View Product
+            </Button>
+          </Box>
         </SwiperSlide>
       </Swiper>
     </Box>
